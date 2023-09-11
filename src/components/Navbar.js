3d8676c6938b9ab31f3ade5b3a117002fc246969
Vraj Prajapati <vrajrp4@gmail.com>
2023-09-11 16:52:11 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,8 +11,7 @@ export default class Navbar extends Component {
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item"><Link className="nav-link" aria-current="page" to="/">Home</Link></li>     
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">    
         <li className="nav-item"><Link className="nav-link" to="/Business">Business</Link></li>
         <li className="nav-item"><Link className="nav-link" to="/Entertainment">Entertainment</Link></li>
         <li className="nav-item"><Link className="nav-link" to="/General">General</Link></li>
